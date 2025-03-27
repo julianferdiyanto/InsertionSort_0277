71fdad8adb25f20ff78c3399ed31445c237dd934
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-03-27 13:25:05 +0700

    menambahkan fungsi display

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -54,3 +54,19 @@ void insertionSort()
         arr[j + 1] = temp; // step 5
     }
 }
+
+void display()
+{
+    cout << endl; // Output baris kosong
+    cout << "=================================" << endl;
+    cout << "Total Pass = " << n - 1 << endl; // count element movement
+    cout << "=================================" << endl; // Output ke layar 
+    cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+    cout << "=================================" << endl; // Output ke layar
+
+    for (int j = 0; j < n; j++)
+    { // Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl; // Output ke layar
+    }
+    cout << endl; // Output baris kosong
+}
