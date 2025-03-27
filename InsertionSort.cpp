e17203d9f4d686bf75041647842d222ac4f2e75b
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-03-27 13:18:34 +0700

    menambahkan fungsi insertionSort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -31,3 +31,26 @@ void input()
         cin >> arr[1]; // Menyimpan nilai data n kedalam array arr
     }
 }
+
+void insertionSort()
+{ // prosedure insertion Sort
+    
+    int temp; // Membuat variable data temporer atau penyimpanan sementara
+    int j, i; // Membuat variable j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+    { // step 1
+
+        temp = arr[1]; // step 2
+
+        j = i - 1; // step 3
+
+        while (j >= 0 && arr[j] > temp) // step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j--; // step 4b
+        }
+
+        arr[j + 1] = temp; // step 5
+    }
+}
