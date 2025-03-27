f1687f018d07ae3182ae34cc11e40db5e69cdd1c
julianferdiyanto <julianferdiyanto3@gmail.com>
2025-03-27 12:59:10 +0700

    menambahkan fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
+
+void input()
+{ // prosedure input
+    while(true)
+    {
+        cout << "Masukan Jumlah Data pada Array :"; // Membuat inputan jumlah element Array
+        cin >> n; // Memanggil variable inputan n
+    }
+}
